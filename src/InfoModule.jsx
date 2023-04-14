2073ab9df58394e07af504e55c0f4b1b61522006
Max Shu <shushakov@gmail.com>
2023-04-14 15:35:42 +0000

    Update 4 files

--- src/InfoModule.jsx
+++ src/InfoModule.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import "./styles.css";
+import "@flodesk/grain/es/styles/base.css";
+import React from "react";
+import { Arrange, Modal, IconButton, IconInfo } from "@flodesk/grain";
+import { useState } from "react";
 
 export default function InfoModule({ text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={`module ${text.class}`}>
-      <h5 className="module_category">{text.category}</h5>
+      {text.category && (
+        <Arrange justifyContent="space-between">
+          <h5 className="module_category">{text.category}</h5>
+          <IconButton
+            icon={<IconInfo size="s" color="darkseagreen" />}
+            onClick={() => setIsOpen(true)}
+          />
+        </Arrange>
+      )}
       <h3 className="module_title">
         {text.title} {text.titleExtra && <span>{text.titleExtra}</span>}
       </h3>
       {text.ends && <p className="module_time">до {text.ends}</p>}
       <p className="module_description">{text.description}</p>
+      <Modal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        title={text.category}
+        description={"Описание…"}
+      ></Modal>
     </div>
-  )
+  );
 }
