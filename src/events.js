28cd4141cb4deb22e91d7ffe13f5e08bb5358ca3
Max Shu <shushakov@gmail.com>
2023-04-14 11:32:40 +0000

    Update events.js

--- src/events.js
+++ src/events.js
@@ -4,175 +4,175 @@ export const events = [
     start: "2023-09-14T19:40:00-06:00",
     end: "2023-09-14T19:40:00-06:00",
     classNames: "newmoon",
-    color: "#2d4464"
+    color: "#2d4464",
   },
   {
     title: "Новолуние",
     start: "2023-02-20T00:06:00-07:00",
     end: "2023-02-20T00:06:00-07:00",
     classNames: "newmoon",
-    color: "#2d4464"
+    color: "#2d4464",
   },
   {
     title: "Полнолуние",
     start: "2023-12-26T17:33:00-07:00",
     end: "2023-12-26T17:33:00-07:00",
     classNames: "fullmoon",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
     title: "Полнолуние",
     start: "2023-01-06T16:08:00-07:00",
     end: "2023-01-06T16:08:00-07:00",
     classNames: "fullmoon",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
     title: "Полнолуние",
     start: "2023-08-01T12:31:00-06:00",
     end: "2023-08-01T12:31:00-06:00",
     classNames: "fullmoon",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
     title: "Новолуние",
     start: "2023-08-16T03:38:00-06:00",
     end: "2023-08-16T03:38:00-06:00",
     classNames: "newmoon",
-    color: "#2d4464"
+    color: "#2d4464",
   },
   {
     title: "Новолуние",
     start: "2023-07-17T12:32:00-06:00",
     end: "2023-07-17T12:32:00-06:00",
     classNames: "newmoon",
-    color: "#2d4464"
+    color: "#2d4464",
   },
   {
     title: "Полнолуние",
     start: "2023-03-07T05:40:00-07:00",
     end: "2023-03-07T05:40:00-07:00",
     classNames: "fullmoon",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
     title: "Новолуние",
     start: "2023-01-21T13:53:00-07:00",
     end: "2023-01-21T13:53:00-07:00",
     classNames: "newmoon",
-    color: "#2d4464"
+    color: "#2d4464",
   },
   {
     title: "Полнолуние",
     start: "2023-11-27T02:16:00-07:00",
     end: "2023-11-27T02:16:00-07:00",
     classNames: "fullmoon",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
     title: "Полнолуние",
     start: "2023-08-30T19:35:00-06:00",
     end: "2023-08-30T19:35:00-06:00",
     classNames: "fullmoon",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
     title: "Новолуние",
     start: "2023-05-19T09:53:00-06:00",
     end: "2023-05-19T09:53:00-06:00",
     classNames: "newmoon",
-    color: "#2d4464"
+    color: "#2d4464",
   },
   {
-    title: "Полнолуние",
+    title: "Полнолуние. Лунное затмение",
     start: "2023-10-28T14:24:00-06:00",
     end: "2023-10-28T14:24:00-06:00",
     classNames: "fullmoon eclipse",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
     title: "Новолуние",
     start: "2023-12-12T16:32:00-07:00",
     end: "2023-12-12T16:32:00-07:00",
     classNames: "newmoon",
-    color: "#2d4464"
+    color: "#2d4464",
   },
   {
     title: "Полнолуние",
     start: "2023-09-29T03:57:00-06:00",
     end: "2023-09-29T03:57:00-06:00",
     classNames: "fullmoon",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
-    title: "Новолуние",
+    title: "Новолуние. Солнечное затмение",
     start: "2023-04-19T22:12:00-06:00",
     end: "2023-04-19T22:12:00-06:00",
     classNames: "newmoon eclipse",
-    color: "#2d4464"
+    color: "#2d4464",
   },
   {
-    title: "Новолуние",
+    title: "Новолуние. Солнечное затмение",
     start: "2023-10-14T11:55:00-06:00",
     end: "2023-10-14T11:55:00-06:00",
     classNames: "newmoon eclipse",
-    color: "#2d4464"
+    color: "#2d4464",
   },
   {
     title: "Новолуние",
     start: "2023-11-13T02:27:00-07:00",
     end: "2023-11-13T02:27:00-07:00",
     classNames: "newmoon",
-    color: "#2d4464"
+    color: "#2d4464",
   },
   {
     title: "Полнолуние",
     start: "2023-06-03T21:42:00-06:00",
     end: "2023-06-03T21:42:00-06:00",
     classNames: "fullmoon",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
     title: "Новолуние",
     start: "2023-06-17T22:37:00-06:00",
     end: "2023-06-17T22:37:00-06:00",
     classNames: "newmoon",
-    color: "#2d4464"
+    color: "#2d4464",
   },
   {
     title: "Полнолуние",
     start: "2023-07-03T05:39:00-06:00",
     end: "2023-07-03T05:39:00-06:00",
     classNames: "fullmoon",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
     title: "Новолуние",
     start: "2023-03-21T11:23:00-06:00",
     end: "2023-03-21T11:23:00-06:00",
     classNames: "newmoon",
-    color: "#2d4464"
+    color: "#2d4464",
   },
   {
     title: "Полнолуние",
     start: "2023-04-05T22:35:00-06:00",
     end: "2023-04-05T22:35:00-06:00",
     classNames: "fullmoon",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
     title: "Полнолуние",
     start: "2023-02-05T11:29:00-07:00",
     end: "2023-02-05T11:29:00-07:00",
     classNames: "fullmoon",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
-    title: "Полнолуние",
+    title: "Полнолуние. Лунное затмение",
     start: "2023-05-05T11:34:00-06:00",
     end: "2023-05-05T11:34:00-06:00",
     classNames: "fullmoon eclipse",
-    color: "#ffce00"
+    color: "#ffce00",
   },
   {
     title: "Солнцестояние",
@@ -180,7 +180,7 @@ export const events = [
       "In the northern hemisphere, this marks the first day of summer and is the day with most hours of light in the year -- when the Sun is farthest north. In the southern hemisphere, this is your winter solstice, marking the shortest day of the year.\n\n21 June will be the longest day of 2022 in the northern hemisphere. ",
     start: "2023-06-21T15:58:19-06:00",
     end: "2023-06-21T16:58:19-06:00",
-    classNames: "solstice"
+    classNames: "solstice",
   },
   {
     title: "Равноденствие",
@@ -188,7 +188,7 @@ export const events = [
       "This is the date when night and day are most nearly the same length. If you live in the northern hemisphere, happy first day of spring! In the southern hemisphere, the vernal equinox corresponds to the first day of fall.",
     start: "2023-03-20T15:24:00-06:00",
     end: "2023-03-20T16:24:00-06:00",
-    classNames: "equinox"
+    classNames: "equinox",
   },
   {
     title: "Равноденствие",
@@ -196,7 +196,7 @@ export const events = [
       "The Sun will beam down directly on the equator giving us just about equal amounts of day and night in most parts of the world. North of the equator, this is your Autumnal Equinox. If you live south of the equator, this is your Spring Equinox.\n\nAlso, try balancing an egg on its end! Whether or not you succeed has nothing to do with the Equinox, but it's still a fun game to try on any day of the year. :)",
     start: "2023-09-24T00:50:00-06:00",
     end: "2023-09-24T01:50:00-06:00",
-    classNames: "equinox"
+    classNames: "equinox",
   },
   {
     title: "Солнцестояние",
@@ -204,6 +204,6 @@ export const events = [
       "This marks the beginning of winter (in the northern hemisphere) and the beginning of summer (in the southern hemisphere.) The winter solstice is the shortest day of the year, respectively, in the sense that the length of time elapsed between sunrise and sunset on this day is a minimum for the year. Of course, daylight saving time means that the first Sunday in April has 23 hours and the last Sunday in October has 25 hours, but these human meddlings with the calendar and do not correspond to the actual number of daylight hours. \n\nIf you live in the southern hemisphere, this is your Summer Solstice, and good cause for celebrating the longest day of the year.",
     start: "2023-12-21T20:27:38-07:00",
     end: "2023-12-21T21:27:38-07:00",
-    classNames: "solstice"
-  }
+    classNames: "solstice",
+  },
 ];
