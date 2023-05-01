9df0c80783141ff0c147fb3e26b69d2a630807a3
Max Shu <shushakov@gmail.com>
2023-05-01 10:54:47 +0000

    Update TaraCalendar.jsx

--- src/components/TaraCalendar.jsx
+++ src/components/TaraCalendar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useRef } from "react";
 import FullCalendar from "@fullcalendar/react";
 import multiMonthPlugin from "@fullcalendar/multimonth";
 import interactionPlugin from "@fullcalendar/interaction";
