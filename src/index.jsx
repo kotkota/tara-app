e9d7a6786d1e1b2de6f331f0ba9e86e825ed098e
Max Shu <shushakov@gmail.com>
2023-05-13 12:19:14 +0000

    Update 4 files

--- src/index.jsx
+++ src/index.jsx
@@ -5,4 +5,17 @@ import App from "./App";
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then((registration) => {
+        console.log("Service worker registered:", registration);
+      })
+      .catch((error) => {
+        console.log("Service worker registration failed:", error);
+      });
+  });
+}
+
 root.render(<App />);
