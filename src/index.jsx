62fbcd3e149c5fb01b33b2e2357e666c5c2ccaa8
Max Shu <shushakov@gmail.com>
2023-05-14 15:45:23 +0000

    Update 8 files

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,13 @@
 import * as ReactDOMClient from "react-dom/client";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 import App from "./App";
 
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./service-worker.js")
-      .then((registration) => {
-        console.log("Service worker registered:", registration);
-      })
-      .catch((error) => {
-        console.log("Service worker registration failed:", error);
-      });
-  });
-}
-
 root.render(<App />);
+
+serviceWorkerRegistration.unregister();
+reportWebVitals();
