a25e530fce6474eea0629759194b6cbdb586f923
Max Shu <bobcat@MacBook-Pro-Max.local>
2026-01-05 02:44:13 +0800

    revert sankranti table

--- src/data/sankranti.js
+++ src/data/sankranti.js
@@ -1,62 +1,62 @@
 export const sankranti = [
   {
     title: "Переход Солнца в знак Козерог",
-    start: "2026-01-14T03:25:00.000Z",
+    start: "2025-01-14T03:25:00.000Z",
     classNames: "suryasankranti",
   },
   {
     title: "Переход Солнца в знак Водолей",
-    start: "2026-02-12T16:25:00.000Z",
+    start: "2025-02-12T16:25:00.000Z",
     classNames: "suryasankranti",
   },
   {
     title: "Переход Солнца в знак Рыбы",
-    start: "2026-03-14T13:20:00.000Z",
+    start: "2025-03-14T13:20:00.000Z",
     classNames: "suryasankranti",
   },
   {
     title: "Переход Солнца в знак Овен",
-    start: "2026-04-13T22:51:00.000Z",
+    start: "2025-04-13T22:51:00.000Z",
     classNames: "suryasankranti",
   },
   {
     title: "Переход Солнца в знак Телец",
-    start: "2026-05-14T19:41:00.000Z",
+    start: "2025-05-14T19:41:00.000Z",
     classNames: "suryasankranti",
   },
   {
     title: "Переход Солнца в знак Близнецы",
-    start: "2026-06-15T02:13:00.000Z",
+    start: "2025-06-15T02:13:00.000Z",
     classNames: "suryasankranti",
   },
   {
     title: "Переход Солнца в знак Рак",
-    start: "2026-07-16T13:01:00.000Z",
+    start: "2025-07-16T13:01:00.000Z",
     classNames: "suryasankranti",
   },
   {
     title: "Переход Солнца в знак Лев",
-    start: "2026-08-16T21:21:00.000Z",
+    start: "2025-08-16T21:21:00.000Z",
     classNames: "suryasankranti",
   },
   {
     title: "Переход Солнца в знак Дева",
-    start: "2026-09-16T21:16:00.000Z",
+    start: "2025-09-16T21:16:00.000Z",
     classNames: "suryasankranti",
   },
   {
     title: "Переход Солнца в знак Весы",
-    start: "2026-10-17T09:15:00.000Z",
+    start: "2025-10-17T09:15:00.000Z",
     classNames: "suryasankranti",
   },
   {
     title: "Переход Солнца в знак Скорпион",
-    start: "2026-11-16T08:06:00.000Z",
+    start: "2025-11-16T08:06:00.000Z",
     classNames: "suryasankranti",
   },
   {
     title: "Переход Солнца в знак Стрелец",
-    start: "2026-12-15T22:48:00.000Z",
+    start: "2025-12-15T22:48:00.000Z",
     classNames: "suryasankranti",
   },
   {
